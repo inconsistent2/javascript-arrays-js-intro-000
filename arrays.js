2eb92feac1604df0c23bb037cac898a69e7e0ebd
Jon Whithouse <flatironschool@foxlawn.plus.com>
2020-05-12 09:59:39 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,8 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray (myArray,addition) {
-[addition,...myArray];
-return myArray
+return[addition,...myArray];
+//return myArray
 }
 
 function destructivelyAddElementToBeginningOfArray (myArray,addition) {
